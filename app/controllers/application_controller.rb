5eb6032ba26325a9f3eebd0802b4e74a13f103bb
sophiawennstedt <sophia.wennstedt@kellogg.northwestern.edu>
2020-05-13 17:51:34 +0000

    square form created doesnt work yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({:template => "calc_temp/square_form.html.erb"})
+  end
+
+  # def blank_square_form
+  #   render({:template => "calc_temp/square_form.html.erb"})
+  # end
+
+  # def blank_square_form
+  #   render({:template => "calc_temp/square_form.html.erb"})
+  # end
+
+  # def blank_square_form
+  #   render({:template => "calc_temp/square_form.html.erb"})
+  # end
+
 end
