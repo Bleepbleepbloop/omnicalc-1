067a3ad56ea14d2586547f5d2d90ce054d051cab
sophiawennstedt <sophia.wennstedt@kellogg.northwestern.edu>
2020-05-13 18:22:14 +0000

    square form and result work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,12 @@ class ApplicationController < ActionController::Base
     render({:template => "calc_temp/square_form.html.erb"})
   end
 
-  # def blank_square_form
-  #   render({:template => "calc_temp/square_form.html.erb"})
-  # end
+  def calc_square
+    @num = params.fetch("user_input_sq").to_i
+    @sq_num = @num ** 2
+
+    render({:template => "calc_temp/square_result.html.erb"})
+  end
 
   # def blank_square_form
   #   render({:template => "calc_temp/square_form.html.erb"})
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-    get("/square/new", {:controller => "application", :action => "blank_square_form"})
+  get("/square/new", {:controller => "application", :action => "blank_square_form"})
+  get("/square/result", {:controller => "application", :action => "calc_square"})
   # Write your routes above this line
   # =================================
 
